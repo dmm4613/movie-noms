cdeb5fdfbd34abb17b1387d89053d06229584c6d
dmm4613 <dmm4613@gmail.com>
2021-01-15 16:08:12 -0500

    nominated button retains its state if when new search is completed

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -5,38 +5,46 @@ const genericPoster = 'https://s.studiobinder.com/wp-content/uploads/2017/12/Mov
 const MovieList = (props) => {
 
     const [movieNoms, setMovieNoms] = useState([]);
-    // const [btnDisabled, setbtnDisabled] = useState(false);
     
     function handleAdd(event) {
         let movie = JSON.parse(event.target.value);
 
+        if (movieNoms.length === 4) {
+            document.getElementById('search-list').classList.add('max-reached');
+            // document.querySelectorAll('button.movie-nom-btn')
+            //     .forEach((e) => {e.setAttribute("disabled", "disabled");});  possible to come back to and disable all buttons 
+        }
+
         if (!movieNoms.some(e => e.imdbID === movie.imdbID)){
             if (movieNoms.length < 5){
                 setMovieNoms(movieNoms => [...movieNoms, movie]);
-                document.getElementById(`${movie.imdbID}-btn`).setAttribute("disabled", "disabled");
             }
-            else {
-                window.alert("You have nominated 5 movies")
-            }
+            
         }
     }
 
     function handleRemove(event) {
+        if (movieNoms.length === 5) {
+            document.getElementById('search-list').classList.remove('max-reached');
+            // document.querySelectorAll('button.movie-nom-btn')
+            //     .forEach((e) => {e.removeAttribute("disabled");}); need to come back to this if it doesn't reset all buttons.
+        }
         const nomList = movieNoms.filter((item) => item.imdbID !== event.target.value);
-        document.getElementById(`${event.target.value}-btn`).removeAttribute("disabled");
+        // document.getElementById(`${event.target.value}-btn`).removeAttribute("disabled");
         setMovieNoms(nomList);
     }
 
     return (
         <>
-        <div className="movie-list d-flex w-100">
+        <div id="search-list" className="movie-list d-flex w-100">
         {props.movies.map((movie) => 
             <div className="d-flex justify-content-start m-4">
                 <div key={movie.imdbID} className="d-flex flex-column justify-content-start movie-card">
                     <img className="movie-poster" src={movie.Poster === 'N/A' ? genericPoster : movie.Poster} alt={`${movie.Title} movie poster`}></img>
                     <h2 className="h4 text-center">{`${movie.Title} (${movie.Year})`}</h2>
-                    <button id={`${movie.imdbID}-btn`} type="button" className="btn btn-success movie-nom-btn" onClick={handleAdd} value={JSON.stringify(movie)}>
-                        Nominate
+                    <button id={`${movie.imdbID}-btn`} type="button" className="btn btn-success movie-nom-btn" onClick={handleAdd} value={JSON.stringify(movie)}
+                     disabled={(movieNoms.some(e => e.imdbID === movie.imdbID))}>
+                         {(movieNoms.some(e => e.imdbID === movie.imdbID)) ? 'Nominated' : 'Nominate'}
                     </button>        
                 </div>
             </div>
@@ -49,7 +57,7 @@ const MovieList = (props) => {
                     <img className="movie-poster" src={movie.Poster === 'N/A' ? genericPoster : movie.Poster} alt={`${movie.Title} movie poster`}></img>
                     <h2 className="h4 text-center">{`${movie.Title} (${movie.Year})`}</h2>
                     <p>{movie.nominated}</p>
-                    <button type="button" className="btn btn-danger movie-nom-btn" onClick={handleRemove} value={movie.imdbID}>Remove</button>
+                    <button type="button" className="btn btn-danger movie-denom-btn" onClick={handleRemove} value={movie.imdbID}>Remove</button>
                 </div>
             </div>
             
